342f29fc0ac7f76c6dde91bf5f4033ebb2d02c81
ClemouDouard <clementleveque911@gmail.com>
2025-05-08 17:08:03 +0200

    add user

--- database/db.py
+++ database/db.py
@@ -3,3 +3,16 @@ import sqlite3
 def get_connection():
     conn = sqlite3.connect('data/db.sqlite')
     return conn
+
+def add_user(username: str, password_hash: str):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        INSERT INTO users (username, password_hash)
+        VALUES (?, ?)
+    ''', (username, password_hash))
+
+    conn.commit()
+    conn.close()
+
